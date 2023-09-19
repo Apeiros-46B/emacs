594dabe5188b02e34b344adc0bb19b72a843242b
Apeiros-46B <Apeiros-46B@users.noreply.github.com>
2023-09-19 09:27:34 -0700

    some changes

--- cfg/keymaps.el
+++ cfg/keymaps.el
@@ -1,17 +1,12 @@
 ; muscle memory
-(defkm 'normal "WW" 'save-buffer)
-(defkm 'normal "C-q" 'evil-visual-block)
-(defkm '(normal visual) "C-j" 'evil-join)
+(defkm 'normal "WW"    'save-buffer)
+(defkm 'normal "C-q"   'evil-visual-block)
 (defkm 'normal "C-S-j" 'eval-print-last-sexp)
 
 ; buffers
-(ldr-defkm "bd" 'kill-current-buffer)
-(ldr-defkm "bh" 'previous-buffer)
-(ldr-defkm "bl" 'next-buffer)
-
-(defkm 'normal "X" 'kill-current-buffer)
-(defkm 'normal "J" 'previous-buffer)
-(defkm 'normal "K" 'next-buffer)
+(ldr-defkm "x" 'kill-current-buffer)
+(ldr-defkm "j" 'previous-buffer)
+(ldr-defkm "k" 'next-buffer)
 
 ; other
-(ldr-defkm "ff" 'dired)
+(ldr-defkm "ff" 'find-file)
--- cfg/pkgs/hop.el
+++ cfg/pkgs/hop.el
@@ -11,10 +11,10 @@
 
   :init
     ; {{{ keymaps
-    (ldr-defkm "hh" 'hop-word)
-    (ldr-defkm "hc" 'hop-char)
-    (ldr-defkm "hn" 'hop-line)
-    (ldr-defkm "hN" 'hop-line-skip-whitespace)
+    (ldr-defkm "Hh" 'hop-word)
+    (ldr-defkm "Hc" 'hop-char)
+    (ldr-defkm "Hn" 'hop-line)
+    (ldr-defkm "HN" 'hop-line-skip-whitespace)
     ; }}}
 
   :config
