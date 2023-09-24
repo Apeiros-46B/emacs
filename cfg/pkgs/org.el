688b66d07fce82292f80105ae7104063e5e09587
Apeiros-46B <Apeiros-46B@users.noreply.github.com>
2023-09-24 10:44:24 -0700

    org definition term face

--- cfg/pkgs/org.el
+++ cfg/pkgs/org.el
@@ -117,7 +117,8 @@
     (set-face-attribute 'org-level-4 nil :foreground (getcol 'purple) :weight 'bold)
     (set-face-attribute 'org-level-5 nil :foreground (getcol 'red)    :weight 'bold)
 
-    (set-face-attribute 'org-link nil :foreground (getcol 'purple) :underline t)
+    (set-face-attribute 'org-link    nil :foreground (getcol 'purple) :underline t)
+    (set-face-attribute 'org-list-dt nil :foreground (getcol 'green))
     ; }}}
 
     ; {{{ org-agenda
